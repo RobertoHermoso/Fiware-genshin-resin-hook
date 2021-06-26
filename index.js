ee9787ee4d8ca15feb8f508f7bb95100afa6bdc5
Roberto Hermoso <rhermoso98@gmail.com>
2021-06-26 16:58:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post("/hook", (req, res) => {
+  console.log(req.body);
   resinService.checkResin(req.body);
   res.status(200).end(); // Responding is important
 });
